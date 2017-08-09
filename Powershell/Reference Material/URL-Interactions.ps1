777b1ee34ce39febe402b79edde405319a044411
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-08-09 13:25:16 -0600

    Adding a test case with a subdomain

--- Powershell/Reference Material/URL-Interactions.ps1
+++ Powershell/Reference Material/URL-Interactions.ps1
@@ -1,5 +1,5 @@
-$someURL = "https://stackoverflow.com/questions/14363214/get-domain-from-url-in-powershell"
-
+#$someURL = "https://stackoverflow.com/questions/14363214/get-domain-from-url-in-powershell"
+$someURL = "https://2017.hackcu.org/live/"
 $parsedURI = [System.Uri]$someURL
 $parsedURI.absoluteUri
 $parsedURI.absolutePath
